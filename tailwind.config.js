396bb7ae19fc0228b3a39580243c84b423e110ea
Getahun Haftu <getahun19h@gmail.com>
2025-02-11 03:47:25 -0600

    tailwind.config.js & .gitignore added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/input.css',  // Scan input.css for Tailwind classes
+    './src/output.css',
+    './index.html',
+    './script.js',
+  ],
+  theme: {
+    extend: {}, // Extend the default theme here
+  },
+  plugins: [], // Add Tailwind plugins here
+}
